c57eaf0630a62c230395586b6376bf9b25931c5e
Rhys Mason-Gorrie <155443676+ThatDigitalGuy@users.noreply.github.com>
2024-10-02 10:54:26 +0000

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import {
+      Button,
+      Card,
+      CardBody,
+      CardFooter,
+      CardHeader,
+      CardSubtitle,
+      CardText,
+      CardTitle,
+      Input
+    } from '@sveltestrap/sveltestrap';
+  </script>
+  
+  <Card>
+    <CardHeader>
+      <CardTitle>Log Hours</CardTitle>
+    </CardHeader>
+    <CardBody>
+      <CardSubtitle>Card subtitle</CardSubtitle>
+      <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+      <Button>Submit</Button>
+    </CardBody>
+  </Card>
