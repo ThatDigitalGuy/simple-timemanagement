08578aff65c34c1a373bf58b8a45f13ebe315c66
Rhys Mason-Gorrie <155443676+ThatDigitalGuy@users.noreply.github.com>
2024-10-03 09:04:18 +0000

    Prod

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,67 @@
 <script lang="ts">
-    import {
-      Button,
-      Card,
-      CardBody,
-      CardFooter,
-      CardHeader,
-      CardSubtitle,
-      CardText,
-      CardTitle,
-      Input
-    } from '@sveltestrap/sveltestrap';
-  </script>
-  
-  <Card>
-    <CardHeader>
-      <CardTitle>Log Hours</CardTitle>
-    </CardHeader>
-    <CardBody>
-      <CardSubtitle>Card subtitle</CardSubtitle>
-      <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-      <Button>Submit</Button>
-    </CardBody>
-  </Card>
+  import {
+    Button,
+    Card,
+    CardBody,
+    CardHeader,
+    CardTitle,
+    Input,
+    FormGroup,
+  } from "@sveltestrap/sveltestrap";
+
+  const handleSubmitLog = async () => {
+    const dateForm = document.getElementById("formDate");
+    const timeFromForm = document.getElementById("formTimeFrom");
+    const timeToForm = document.getElementById("formTimeDone");
+    const whereForm = document.getElementById("whereForm");
+
+    const payload = JSON.stringify({
+      "date": dateForm?.value,
+      "from": timeFromForm?.value,
+      "to": timeToForm?.value,
+      "where": whereForm?.value,
+    });
+
+    console.log(payload)
+
+    const request = await fetch('https://db.rhysmason.uk/api/v2/tables/mmajcep1y0ye0jm/records', {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        'xc-token': "Q8YZuXP7WkAUPFE9-ZnpQ_AAXQ-8JdLPeFAitgDI"
+      },
+      body: payload
+    });
+  };
+</script>
+
+<Card>
+  <CardHeader>
+    <CardTitle>Log Hours</CardTitle>
+  </CardHeader>
+  <CardBody>
+    <div class="p-4">
+      <div class="mb-2">
+        <FormGroup floating label="Date?">
+          <Input id="formDate" type="date" placeholder="date placeholder" />
+        </FormGroup>
+      </div>
+      <div class="mb-2">
+        <FormGroup floating label="When did you start?">
+          <Input id="formTimeFrom" type="time" placeholder="date placeholder" />
+        </FormGroup>
+      </div>
+      <div class="mb-2">
+        <FormGroup floating label="When did you finish?">
+          <Input id="formTimeDone" type="time" placeholder="date placeholder" />
+        </FormGroup>
+      </div>
+      <div class="mb-2">
+        <FormGroup floating label="Where?">
+          <Input id="whereForm" placeholder="Where?" />
+        </FormGroup>
+      </div>
+    </div>
+    <Button on:click={handleSubmitLog}>Submit</Button>
+  </CardBody>
+</Card>
---
+++ src/routes/history/+page.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    import { Table } from '@sveltestrap/sveltestrap';
+    const request = fetch('https://db.rhysmason.uk/api/v2/tables/mmajcep1y0ye0jm/records', {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        'xc-token': "Q8YZuXP7WkAUPFE9-ZnpQ_AAXQ-8JdLPeFAitgDI"
+      }
+    });
+  </script>
+  
+<div class="p-8">
+    <Table bordered>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+</div>
